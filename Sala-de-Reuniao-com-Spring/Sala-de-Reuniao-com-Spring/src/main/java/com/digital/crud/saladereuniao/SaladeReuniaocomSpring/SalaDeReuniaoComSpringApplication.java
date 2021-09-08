fd7d0df91391e016ee4e366c1ff28b0bbc2d6c21
Sandro Oliveira <89020876+Sandroteolive@users.noreply.github.com>
2021-09-08 09:35:36 -0300

    Add files via upload

---
+++ Sala-de-Reuniao-com-Spring/Sala-de-Reuniao-com-Spring/src/main/java/com/digital/crud/saladereuniao/SaladeReuniaocomSpring/SalaDeReuniaoComSpringApplication.java
@@ -0,0 +1,13 @@
+package com.digital.crud.saladereuniao.SaladeReuniaocomSpring;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SalaDeReuniaoComSpringApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SalaDeReuniaoComSpringApplication.class, args);
+	}
+
+}
---
+++ Sala-de-Reuniao-com-Spring/Sala-de-Reuniao-com-Spring/src/test/java/com/digital/crud/saladereuniao/SaladeReuniaocomSpring/SalaDeReuniaoComSpringApplicationTests.java
@@ -0,0 +1,13 @@
+package com.digital.crud.saladereuniao.SaladeReuniaocomSpring;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SalaDeReuniaoComSpringApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
